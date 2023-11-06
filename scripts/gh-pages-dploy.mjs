b0cc4219a5bc52e16975066bdaed29453916b90c
Zidan Hadipratama <zzzdn.hadi@gmail.com>
2023-11-06 22:08:46 +0700

    uab hdikit

---
+++ scripts/gh-pages-dploy.mjs
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+
+// Import 'execa' using import statement
+import { execa } from 'execa';
+import fs from 'fs/promises'; // Use 'fs/promises' for async file operations
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = await fs.access("dist").then(() => "dist").catch(() => "build");
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    // Use 'rmdir' instead of 'rm' for Windows
+    await execa("rmdir", ["/s", "/q", folderName]);
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
